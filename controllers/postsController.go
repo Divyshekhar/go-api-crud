7eeb295d2b51898190794dc9ecab829c3d41de81
Divyshekhar Sinha <divyshekhar2204@gmail.com>
2025-05-19 13:00:21 +0530

    init

---
+++ controllers/postsController.go
@@ -0,0 +1,81 @@
+package controllers
+
+import (
+	"github.com/Divyshekhar/go-crud/initializers"
+	"github.com/Divyshekhar/go-crud/models"
+	"github.com/gin-gonic/gin"
+)
+
+func PostsCreate(c *gin.Context) {
+	var body struct {
+		Body  string `json:"body"`
+		Title string `json:"title"`
+	}
+
+	err := c.Bind(&body)
+	if err != nil {
+		c.JSON(401, gin.H{
+			"message": "wrong type of Data",
+		})
+	}
+
+	post := models.Post{Title: body.Title, Body: body.Body}
+
+	result := initializers.DB.Create(&post)
+	if result.Error != nil {
+		c.Status(400)
+		return
+	}
+	c.JSON(200, gin.H{
+		"post": post,
+	})
+
+}
+func PostsIndex(c *gin.Context) {
+	//get the posts
+	var posts []models.Post
+	initializers.DB.Find(&posts)
+
+	c.JSON(200, gin.H{
+		"posts": posts,
+	})
+
+	//response with them
+}
+func PostsShow(c *gin.Context) {
+	id := c.Param("id")
+	var post models.Post
+	initializers.DB.First(&post, id)
+	c.JSON(200, gin.H{
+		"post": post,
+	})
+}
+func PostsUpdate(c *gin.Context) {
+	id := c.Param("id")
+
+	var body struct {
+		Body  string
+		Title string
+	}
+	c.Bind(&body)
+	var post models.Post
+	initializers.DB.First(&post, id)
+
+	initializers.DB.Model(&post).Updates(models.Post{
+		Title: body.Title,
+		Body:  body.Body,
+	})
+	c.JSON(200, gin.H{
+		"updated Post": post,
+	})
+}
+func PostsDelete(c *gin.Context) {
+	id := c.Param("id")
+
+	initializers.DB.Delete(&models.Post{}, id)
+
+	c.JSON(200, gin.H{
+		"deleted": "Deleted succesfully",
+	})
+
+}
